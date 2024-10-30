5a5a4f4e623cf2a33945cd675e63ad92f6a0f49b
Andrey <andrei_chesanov@mail.ru>
2024-10-30 19:49:52 +0300

    step4

---
+++ compare_files/scripts/__init__.py
@@ -0,0 +1,3 @@
+from compare_files.main_code_to_skripts.main_code import generate_diff
+
+__all__ = ('generate_diff')
--- compare_files/scripts/gendiff.py
+++ compare_files/scripts/gendiff.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 from compare_files.main_code_to_skripts import main_code
-from compare_files.main_code_to_skripts import read_json
 
 def main():
-    main_code.scripts_for_diff()
-    read_json.read_json()
+    main_code.generate_diff()
+
 
 
 if __name__ == '__main__':
